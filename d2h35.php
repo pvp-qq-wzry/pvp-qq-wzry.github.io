6ce6f97cc94940a44b69759075cbd2d661fa60ea
pvp-qq-wzry <142261127+pvp-qq-wzry@users.noreply.github.com>
2023-08-25 10:18:31 +0800

    Add files via upload

---
+++ d2h35.php
@@ -0,0 +1,266 @@
+﻿
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+
+<title>7x24小时自动发货商城</title>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+
+<style type="text/css">
+body{
+ margin:0px;
+ padding:0px;
+}
+.d1{
+position:absolute; 
+ left:0px;
+ top:0px; 
+ width:100%; 
+ height:100%;
+ z-index:-1;
+}
+</style>
+<body>
+<div class="d1"><img src="https://pic1.zhimg.com/80/v2-0a347e7d106093608a82590bdca90c4f_720w.png" width="100%" height="100%"/></div>
+<center>
+<h1> ‭</h1>
+<h1> ‭</h1></center>
+</p> 
+
+</body>
+</html>
+
+	<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<style>
+*{
+	margin:0;
+	padding:0;
+
+}
+ul,ol{
+	list-style:none;
+
+}
+.titlee{
+    color: #ffffff;
+    font-size: 50px;
+    font-weight: bold;
+    padding: 8px 16px 5px 10px;
+
+}
+.hidden{
+	display:none;
+}
+
+.new-btn-login-sp{
+	border:1px solid #D74C00;
+	padding:1px;
+	display:inline-block;
+
+
+}
+
+.new-btn-login{
+    background-color: #ff8c00;
+	color: #FFFFFF;
+    font-weight: bold;
+	border: medium none;
+	width:380px;
+	height:64px;
+font-size: 36px;
+}
+.new-btn-login:hover{
+    background-color: #ffa300;
+	width: 380px;
+	color: #FFFFFF;
+    font-weight: bold;
+    height: 64px;
+}
+.bank-list{
+	overflow:hidden;
+	margin-top:5px;
+
+}
+.bank-list li{
+	float:left;
+	width:53px;
+	margin-bottom:5px;
+
+}
+
+#main{
+	width:850px;
+	margin:0 auto;
+	font-size:24px;
+	font-family:'宋体';
+}
+#logo{
+	background-color: transparent;
+    background-image: url("sdk/1p/qun.png");
+    border: medium none;
+	background-position:0 0;
+	width:166px;
+	height:35px;
+
+    float:left;
+}
+.red-star{
+	color:#f00;
+	width:10px;
+
+	display:inline-block;
+}
+.null-star{
+	color:#fff;
+
+}
+.content{
+	margin-top:48px;
+font-size: 28px;
+
+	
+}
+
+.content dt{
+	width:160px;
+	display:inline-block;
+	text-align:right;
+	float:left;
+
+}
+.content dd{
+	margin-left:80px;
+	margin-bottom:34px;
+        padding-left: 80px;
+
+}
+#foot{
+	margin-top:20px;
+
+}
+.foot-ul li {
+	text-align:center;
+}
+.note-help {
+    color: #ffffff;
+    font-size: 26px;
+    line-height: 130%;
+    padding-left: 3px;
+
+}
+
+.cashier-nav {
+    font-size: 24px;
+    margin: 24px 0 10px;
+    text-align: left;
+    height:30px;
+    border-bottom:solid 5px #CFD2D7;
+}
+.cashier-nav ol li {
+    float: left;
+}
+.cashier-nav li.current {
+    color: #ffffff;
+    font-weight: bold;
+}
+.cashier-nav li.last {
+    clear:right;
+}
+.alipay_link {
+    text-align:right;
+}
+.alipay_link a:link{
+    text-decoration:none;
+    color:#8D8D8D;
+}
+.alipay_link a:visited{
+    text-decoration:none;
+    color:#8D8D8D;
+
+}
+</style>
+</head>
+<body text=#ffffff bgColor=#000000 leftMargin=0 topMargin=4>
+	<div id="main">
+		<div id="head">
+            <dl class="alipay_link">
+
+            </dl>
+            <span class="titlee">24小时自动发货商城</span>
+		</div>
+        <div class="cashier-nav">
+            <ol>
+				<li class="current">1、选择支付方式 →</li>
+				<li>2、购买注册码 →</li>
+				<li class="last">3、付款完毕自动发货返回网站复制注册码</li>
+            </ol>
+        </div>
+        <form name=alipayment action=epayapi.php method=post target="_blank">
+            <div id="body" style="clear:left">
+                <dl class="content">
+                    <dt>订单编号：</dt>
+                    <dd>
+                        <span class="null-star"></span>
+                        <input readonly="readonly"style="height:50px ;width:600px ;font-size: 28px;>" name="WIDout_trade_no" value="20230823214719411"/>
+<br/>
+<br/>
+<p>
+
+</p>
+                        <span>
+</span>
+                    </dd>
+                    <dt>商品名称：</dt>
+                    <dd>
+                        <span class="null-star"></span>
+                        <input readonly="readonly"style="height:50px ;width:600px ;font-size: 28px;>" name="WIDsubject" value="软件注册码"/>
+
+
+ 
+</p>
+
+</span>
+                    </dd>
+                    <dt>付款金额：</dt>
+                    <dd>
+                        <span class="null-star"></span>
+                        <input readonly="readonly"style="height:50px ;width:600px ;font-size: 28px;>" name="WIDtotal_fee" value="35.00"/>
+                        <span>
+</span>
+                    </dd>
+					<dt>支付方式：</dt>
+                    <dd>
+                  <label><input type="radio" name="type" value="qqpay" checked="">QQ钱包支付</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ←选择支付方式
+                    </dd>
+                  
+                   
+					<dt></dt>
+                    <dd>
+                        <span class="new-btn-login-sp">
+                            <button class="new-btn-login" type="submit" style="text-align:center;">购买注册码</button>
+                        </span>
+                    </dd>
+                </dl>
+            </div>
+		</form>
+
+        <div id="foot">
+			<ul class="foot-ul">
+				<li><font class="note-help">如果您需要微信支付请将微信余额转到QQ再选择QQ钱包支付！</font></li>
+				<li>
+				
+				</li>
+
+        <div id="foot">
+			<ul class="foot-ul">
+				<li><font class="note-help"><a href="./wxzqjc/wxzq.html" style="font-size:38px;color:#dafa05">点击查看微信余额转到QQ钱包教程</a></font></li>
+				<li>
+				</li>
+		</ul>
+		</div>
+
+
+</body>
+</html>
